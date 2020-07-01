c4bcc69d061d8c28aad1a5ce73155c153a3a10fe
Greg Fodor <gfodor@gmail.com>
2020-07-01 13:25:27 -0700

    Actually load one texture per 2 frames

--- src/loaders/HubsTextureLoader.js
+++ src/loaders/HubsTextureLoader.js
@@ -1,6 +1,11 @@
+import nextTick from "../utils/next-tick";
+const MIN_FRAMES_BETWEEN_TEXTURE_UPLOADS = 2;
+
 function loadAsync(loader, url, onProgress) {
   return new Promise((resolve, reject) => loader.load(url, resolve, onProgress, reject));
 }
+
+let nextUploadFrame = 0;
 
 export default class HubsTextureLoader {
   static crossOrigin = "anonymous";
@@ -34,7 +39,14 @@ export default class HubsTextureLoader {
 
     const resolvedUrl = this.manager.resolveURL(src);
 
-    texture.image = await loadAsync(imageLoader, resolvedUrl, onProgress);
+    const image = await loadAsync(imageLoader, resolvedUrl, onProgress);
+
+    // Upload one texture per frame
+    const frameScheduler = AFRAME.scenes[0].systems["frame-scheduler"];
+    while (nextUploadFrame > frameScheduler.frameIndex) await nextTick();
+    nextUploadFrame = frameScheduler.frameIndex + MIN_FRAMES_BETWEEN_TEXTURE_UPLOADS;
+
+    texture.image = image;
 
     // Image was just added to cache before this function gets called, disable caching by immediatly removing it
     THREE.Cache.remove(resolvedUrl);
@@ -46,6 +58,8 @@ export default class HubsTextureLoader {
       texture.image.close && texture.image.close();
       delete texture.image;
     };
+
+    AFRAME.scenes[0].renderer.initTexture(texture);
 
     return texture;
   }
