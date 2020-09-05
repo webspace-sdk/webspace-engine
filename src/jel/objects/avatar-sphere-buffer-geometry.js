ee41a62b80f497693d47e12383e18e1477144abd
Greg Fodor <gfodor@gmail.com>
2020-09-04 17:10:16 -0700

    Add highlight and new buffer geo

---
+++ src/jel/objects/avatar-sphere-buffer-geometry.js
@@ -0,0 +1,111 @@
+const { BufferGeometry, Float32BufferAttribute, Vector3 } = THREE;
+
+const MAP_COORD_UPPER = 0;
+const MAP_COORD_LOWER = 1;
+const MAP_COORD_POLES = 2;
+
+function AvatarSphereBufferGeometry(radius, invert = false) {
+  BufferGeometry.call(this);
+
+  this.type = "SphereBufferGeometry";
+
+  radius = radius || 1;
+
+  const widthSegments = 30;
+  const heightSegments = 30;
+  const mapSegmentLength = Math.floor(heightSegments / 3);
+
+  const phiStart = 0;
+  const phiLength = Math.PI * 2;
+
+  const thetaStart = 0;
+  const thetaLength = Math.PI;
+
+  const thetaEnd = Math.PI;
+
+  let ix, iy;
+
+  let index = 0;
+  const grid = [];
+
+  const vertex = new Vector3();
+  const normal = new Vector3();
+
+  // buffers
+
+  const indices = [];
+  const vertices = [];
+  const normals = [];
+  const uvs = [];
+
+  // generate vertices, normals and uvs
+
+  for (iy = 0; iy <= heightSegments; iy++) {
+    const verticesRow = [];
+
+    const v = iy / heightSegments;
+
+    // special case for the poles
+
+    let uOffset = 0;
+
+    if (iy == 0 && thetaStart == 0) {
+      uOffset = 0.5 / widthSegments;
+    } else if (iy == heightSegments && thetaEnd == Math.PI) {
+      uOffset = -0.5 / widthSegments;
+    }
+
+    for (ix = 0; ix <= widthSegments; ix++) {
+      const u = ix / widthSegments;
+
+      // vertex
+
+      vertex.x = -radius * Math.cos(phiStart + u * phiLength) * Math.sin(thetaStart + v * thetaLength);
+      vertex.y = radius * Math.cos(thetaStart + v * thetaLength);
+      vertex.z = radius * Math.sin(phiStart + u * phiLength) * Math.sin(thetaStart + v * thetaLength);
+
+      vertices.push(vertex.x, vertex.y, vertex.z);
+
+      // normal
+
+      normal.copy(vertex).normalize();
+
+      const n = invert ? -1 : 1;
+      normals.push(normal.x * n, normal.y * n, normal.z * n);
+
+      // uv
+
+      uvs.push(u + uOffset, 1 - v);
+
+      verticesRow.push(index++);
+    }
+
+    grid.push(verticesRow);
+  }
+
+  // indices
+
+  for (iy = 0; iy < heightSegments; iy++) {
+    for (ix = 0; ix < widthSegments; ix++) {
+      const a = grid[iy][ix + 1];
+      const b = grid[iy][ix];
+      const c = grid[iy + 1][ix];
+      const d = grid[iy + 1][ix + 1];
+
+      if (iy !== 0 || thetaStart > 0) indices.push(...(invert ? [d, b, a] : [a, b, d]));
+      if (iy !== heightSegments - 1 || thetaEnd < Math.PI) indices.push(...(invert ? [d, c, b] : [b, c, d]));
+    }
+  }
+
+  // build geometry
+
+  this.setIndex(indices);
+  this.setAttribute("position", new Float32BufferAttribute(vertices, 3));
+  this.setAttribute("normal", new Float32BufferAttribute(normals, 3));
+  this.setAttribute("uv", new Float32BufferAttribute(uvs, 2));
+}
+
+AvatarSphereBufferGeometry.prototype = Object.create(BufferGeometry.prototype);
+AvatarSphereBufferGeometry.prototype.constructor = AvatarSphereBufferGeometry;
+
+export { AvatarSphereBufferGeometry };
--- src/jel/systems/avatar-system.js
+++ src/jel/systems/avatar-system.js
@@ -1,6 +1,7 @@
 import { DynamicInstancedMesh } from "../objects/DynamicInstancedMesh";
 import { RENDER_ORDER } from "../../hubs/constants";
 import { addVertexCurvingToShader } from "./terrain-system";
+import { AvatarSphereBufferGeometry } from "../objects/avatar-sphere-buffer-geometry";
 
 let toonGradientMap;
 
@@ -30,7 +31,6 @@ const {
   ShaderLib,
   //Float32BufferAttribute,
   UniformsUtils,
-  SphereBufferGeometry,
   //Uint16BufferAttribute,
   //Uint32BufferAttribute,
   //LOD,
@@ -39,7 +39,8 @@ const {
 
 const IDENITTY = new Matrix4();
 const AVATAR_RADIUS = 0.4;
-const OUTLINE_SIZE = 0.1;
+const OUTLINE_SIZE = 0.05;
+const HIGHLIGHT_SIZE = 0.15;
 
 const avatarMaterial = new ShaderMaterial({
   name: "avatar",
@@ -65,20 +66,16 @@ avatarMaterial.stencilFunc = THREE.AlwaysStencilFunc;
 avatarMaterial.stencilRef = 2;
 avatarMaterial.stencilZPass = THREE.ReplaceStencilOp;
 
-const outlineMaterial = new MeshBasicMaterial({
-  color: new Color(0, 0, 0),
-  side: THREE.BackSide // TODO generate mesh with inward normals
-});
+const outlineMaterial = new MeshBasicMaterial({ color: new Color(0, 0, 0) });
+const highlightMaterial = new MeshBasicMaterial({ color: new Color(1, 1, 1) });
 
 avatarMaterial.onBeforeCompile = shader => {
   addVertexCurvingToShader(shader);
   //shader.vertexShader = shader.vertexShader.replace("#include <color_vertex>", "vColor.xyz = color.xyz / 255.0;");
 };
 
-outlineMaterial.onBeforeCompile = shader => {
-  addVertexCurvingToShader(shader);
-  //shader.vertexShader = shader.vertexShader.replace("#include <color_vertex>", "vColor.xyz = color.xyz / 255.0;");
-};
+outlineMaterial.onBeforeCompile = shader => addVertexCurvingToShader(shader);
+highlightMaterial.onBeforeCompile = shader => addVertexCurvingToShader(shader);
 
 const MAX_AVATARS = 128;
 
@@ -103,6 +100,7 @@ export class AvatarSystem {
   register(el) {
     const index = this.mesh.addMatrix(IDENITTY);
     this.outlineMesh.addMatrix(IDENITTY);
+    this.highlightMesh.addMatrix(IDENITTY);
 
     this.maxRegisteredIndex = Math.max(index, this.maxRegisteredIndex);
     this.avatarEls[index] = el;
@@ -115,6 +113,7 @@ export class AvatarSystem {
         this.avatarEls[i] = null;
         this.mesh.removeMatrix(i);
         this.outlineMesh.removeMatrix(i);
+        this.highlightMesh.removeMatrix(i);
         return;
       }
     }
@@ -130,19 +129,27 @@ export class AvatarSystem {
   }
 
   createMesh() {
-    this.mesh = new DynamicInstancedMesh(new SphereBufferGeometry(AVATAR_RADIUS, 30, 30), avatarMaterial, 128);
+    this.mesh = new DynamicInstancedMesh(new AvatarSphereBufferGeometry(AVATAR_RADIUS, 30, 30), avatarMaterial, 128);
     this.mesh.renderOrder = RENDER_ORDER.INSTANCED_AVATAR;
     this.mesh.castShadow = true;
 
     this.outlineMesh = new DynamicInstancedMesh(
-      new SphereBufferGeometry(AVATAR_RADIUS + AVATAR_RADIUS * OUTLINE_SIZE, 30, 30),
+      new AvatarSphereBufferGeometry(AVATAR_RADIUS + AVATAR_RADIUS * OUTLINE_SIZE, 30, 30, true),
       outlineMaterial,
       128
     );
     this.outlineMesh.renderOrder = RENDER_ORDER.INSTANCED_AVATAR + 1;
 
+    this.highlightMesh = new DynamicInstancedMesh(
+      new AvatarSphereBufferGeometry(AVATAR_RADIUS + AVATAR_RADIUS * HIGHLIGHT_SIZE, 30, 30, true),
+      highlightMaterial,
+      128
+    );
+    this.highlightMesh.renderOrder = RENDER_ORDER.INSTANCED_AVATAR + 2;
+
     this.sceneEl.object3D.add(this.mesh);
     this.sceneEl.object3D.add(this.outlineMesh);
+    this.sceneEl.object3D.add(this.highlightMesh);
   }
 
   tick() {
@@ -162,6 +169,9 @@ export class AvatarSystem {
 
       this.outlineMesh.setMatrixAt(i, head.matrixWorld);
       this.outlineMesh.instanceMatrix.needsUpdate = true;
+
+      this.highlightMesh.setMatrixAt(i, head.matrixWorld);
+      this.highlightMesh.instanceMatrix.needsUpdate = true;
 
       this.atmosphereSystem.updateShadows();
 
