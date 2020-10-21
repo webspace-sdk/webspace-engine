01747a7b8bb43b07a771d506b4836e7517063e85
Greg Fodor <gfodor@gmail.com>
2020-10-21 16:41:04 -0700

    Add color picker

---
+++ src/jel/react-components/color-picker.stories.js
@@ -0,0 +1,28 @@
+import React from "react";
+import sharedStyles from "../assets/stylesheets/shared.scss";
+import classNames from "classnames";
+
+import ColorPicker from "./color-picker";
+
+export const Normal = () => {
+  return (
+    <div
+      className={classNames(sharedStyles.basePanel)}
+      style={{
+        display: "flex",
+        width: "300px",
+        height: "300px",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <div style={{ width: "125px", height: "125px" }}>
+        <ColorPicker onChangeComplete={e => console.log(e)} />
+      </div>
+    </div>
+  );
+};
+
+export default {
+  title: "Color Picker"
+};
