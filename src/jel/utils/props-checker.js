eb4f5df9ca058d3a1246af293af871fffa930f06
Greg Fodor <gfodor@gmail.com>
2020-10-28 11:35:25 -0700

    Add props checker

---
+++ src/jel/utils/props-checker.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default function withPropsChecker(WrappedComponent) {
+  return class PropsChecker extends Component {
+    UNSAFE_componentWillReceiveProps(nextProps) {
+      Object.keys(nextProps)
+        .filter(key => {
+          return nextProps[key] !== this.props[key];
+        })
+        .map(key => {
+          console.log("changed property:", key, "from", this.props[key], "to", nextProps[key]);
+        });
+    }
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+}
