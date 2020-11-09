96100822ac2a0a2cc56dfa0b021d7a5fa678969d
Greg Fodor <gfodor@gmail.com>
2020-11-09 15:26:39 -0800

    Remove AEC hack when no audio being processed

--- src/hubs/systems/audio-system.js
+++ src/hubs/systems/audio-system.js
@@ -6,64 +6,6 @@ import lipSyncWorker from "../../jel/workers/lipsync.worker.js";
 import rnnWasm from "../../jel/wasm/rnnoise-vad-wasm.js";
 const supportsInsertableStreams = !!(window.RTCRtpSender && !!RTCRtpSender.prototype.createEncodedStreams);
 
-async function enableChromeAEC(gainNode) {
-  /**
-   *  workaround for: https://bugs.chromium.org/p/chromium/issues/detail?id=687574
-   *  1. grab the GainNode from the scene's THREE.AudioListener
-   *  2. disconnect the GainNode from the AudioDestinationNode (basically the audio out), this prevents hearing the audio twice.
-   *  3. create a local webrtc connection between two RTCPeerConnections (see this example: https://webrtc.github.io/samples/src/content/peerconnection/pc1/)
-   *  4. create a new MediaStreamDestination from the scene's THREE.AudioContext and connect the GainNode to it.
-   *  5. add the MediaStreamDestination's track  to one of those RTCPeerConnections
-   *  6. connect the other RTCPeerConnection's stream to a new audio element.
-   *  All audio is now routed through Chrome's audio mixer, thus enabling AEC, while preserving all the audio processing that was performed via the WebAudio API.
-   */
-
-  const audioEl = new Audio();
-  audioEl.setAttribute("autoplay", "autoplay");
-  audioEl.setAttribute("playsinline", "playsinline");
-
-  const context = THREE.AudioContext.getContext();
-  const loopbackDestination = context.createMediaStreamDestination();
-  const outboundPeerConnection = new RTCPeerConnection();
-  const inboundPeerConnection = new RTCPeerConnection();
-
-  const onError = e => {
-    console.error("RTCPeerConnection loopback initialization error", e);
-  };
-
-  outboundPeerConnection.addEventListener("icecandidate", e => {
-    inboundPeerConnection.addIceCandidate(e.candidate).catch(onError);
-  });
-
-  inboundPeerConnection.addEventListener("icecandidate", e => {
-    outboundPeerConnection.addIceCandidate(e.candidate).catch(onError);
-  });
-
-  inboundPeerConnection.addEventListener("track", e => {
-    audioEl.srcObject = e.streams[0];
-  });
-
-  try {
-    //The following should never fail, but just in case, we won't disconnect/reconnect the gainNode unless all of this succeeds
-    loopbackDestination.stream.getTracks().forEach(track => {
-      outboundPeerConnection.addTrack(track, loopbackDestination.stream);
-    });
-
-    const offer = await outboundPeerConnection.createOffer();
-    outboundPeerConnection.setLocalDescription(offer);
-    await inboundPeerConnection.setRemoteDescription(offer);
-
-    const answer = await inboundPeerConnection.createAnswer();
-    inboundPeerConnection.setLocalDescription(answer);
-    outboundPeerConnection.setRemoteDescription(answer);
-
-    gainNode.disconnect();
-    gainNode.connect(loopbackDestination);
-  } catch (e) {
-    onError(e);
-  }
-}
-
 export class AudioSystem {
   constructor(sceneEl) {
     sceneEl.audioListener = sceneEl.audioListener || new THREE.AudioListener();
@@ -86,6 +28,8 @@ export class AudioSystem {
     this.analyserLevels = new Uint8Array(this.outboundAnalyser.fftSize);
     this.outboundGainNode.connect(this.outboundAnalyser);
     this.outboundAnalyser.connect(this.mediaStreamDestinationNode);
+    this.aecHackOutboundPeer = null;
+    this.aecHackInboundPeer = null;
 
     const supportsLipSync = this.audioContext.audioWorklet && window.SharedArrayBuffer && supportsInsertableStreams;
 
@@ -102,9 +46,7 @@ export class AudioSystem {
 
       setTimeout(() => {
         if (this.audioContext.state === "running") {
-          if (!AFRAME.utils.device.isMobile() && /chrome/i.test(navigator.userAgent)) {
-            enableChromeAEC(sceneEl.audioListener.gain);
-          }
+          this.applyAECHack();
 
           document.body.removeEventListener("touchend", resume, false);
           document.body.removeEventListener("mouseup", resume, false);
@@ -238,6 +180,8 @@ export class AudioSystem {
         this.audioNodes.set(id, { sourceNode, gainNode, connected: true });
       }
     }
+
+    this.applyAECHack();
   }
 
   disableOutboundAudioStream(id) {
@@ -249,6 +193,9 @@ export class AudioSystem {
         this.audioNodes.set(id, { sourceNode, gainNode, connected: false });
       }
     }
+
+    // Delay disabling AEC hack to avoid rapid oscillations of WebRTC setup if muting/unmuting
+    setTimeout(() => this.applyAECHack(), 5000);
   }
 
   addStreamToOutboundAudio(id, mediaStream) {
@@ -269,6 +216,86 @@ export class AudioSystem {
       nodes.sourceNode.disconnect();
       nodes.gainNode.disconnect();
       this.audioNodes.delete(id);
+    }
+  }
+
+  /**
+   *  workaround for: https://bugs.chromium.org/p/chromium/issues/detail?id=687574
+   *  1. grab the GainNode from the scene's THREE.AudioListener
+   *  2. disconnect the GainNode from the AudioDestinationNode (basically the audio out), this prevents hearing the audio twice.
+   *  3. create a local webrtc connection between two RTCPeerConnections (see this example: https://webrtc.github.io/samples/src/content/peerconnection/pc1/)
+   *  4. create a new MediaStreamDestination from the scene's THREE.AudioContext and connect the GainNode to it.
+   *  5. add the MediaStreamDestination's track  to one of those RTCPeerConnections
+   *  6. connect the other RTCPeerConnection's stream to a new audio element.
+   *  All audio is now routed through Chrome's audio mixer, thus enabling AEC, while preserving all the audio processing that was performed via the WebAudio API.
+   */
+  async applyAECHack() {
+    if (AFRAME.utils.device.isMobile() || !/chrome/i.test(navigator.userAgent)) return;
+    this.audioContext = THREE.AudioContext.getContext();
+    if (this.audioContext.state !== "running") return;
+
+    const hasConnectedNode = !![...this.audioNodes.values()].find(({ connected }) => connected);
+    const gainNode = this.scene.audioListener.gain;
+
+    if (hasConnectedNode) {
+      if (this.aecHackOutboundPeer) return;
+
+      const audioEl = new Audio();
+      audioEl.setAttribute("autoplay", "autoplay");
+      audioEl.setAttribute("playsinline", "playsinline");
+
+      const context = THREE.AudioContext.getContext();
+      const loopbackDestination = context.createMediaStreamDestination();
+      this.aecHackOutboundPeer = new RTCPeerConnection();
+      this.aecHackInboundPeer = new RTCPeerConnection();
+
+      const onError = e => {
+        console.error("RTCPeerConnection loopback initialization error", e);
+      };
+
+      this.aecHackOutboundPeer.addEventListener("icecandidate", e => {
+        this.aecHackInboundPeer.addIceCandidate(e.candidate).catch(onError);
+      });
+
+      this.aecHackInboundPeer.addEventListener("icecandidate", e => {
+        this.aecHackOutboundPeer.addIceCandidate(e.candidate).catch(onError);
+      });
+
+      this.aecHackInboundPeer.addEventListener("track", e => {
+        audioEl.srcObject = e.streams[0];
+      });
+
+      try {
+        //The following should never fail, but just in case, we won't disconnect/reconnect the gainNode unless all of this succeeds
+        loopbackDestination.stream.getTracks().forEach(track => {
+          this.aecHackOutboundPeer.addTrack(track, loopbackDestination.stream);
+        });
+
+        const offer = await this.aecHackOutboundPeer.createOffer();
+        if (!this.aecHackOutboundPeer) return;
+        this.aecHackOutboundPeer.setLocalDescription(offer);
+
+        await this.aecHackInboundPeer.setRemoteDescription(offer);
+        if (!this.aecHackOutboundPeer || !this.aecHackInboundPeer) return;
+
+        const answer = await this.aecHackInboundPeer.createAnswer();
+        if (!this.aecHackOutboundPeer || !this.aecHackInboundPeer) return;
+
+        this.aecHackInboundPeer.setLocalDescription(answer);
+        this.aecHackOutboundPeer.setRemoteDescription(answer);
+
+        gainNode.disconnect();
+        gainNode.connect(loopbackDestination);
+      } catch (e) {
+        onError(e);
+      }
+    } else {
+      if (!this.aecHackOutboundPeer) return;
+      if (this.aecHackOutboundPeer) this.aecHackOutboundPeer.close();
+      if (this.aecHackInboundPeer) this.aecHackInboundPeer.close();
+      this.aecHackOutboundPeer = null;
+      this.aecHackInboundPeer = null;
+      gainNode.disconnect();
     }
   }
 }
