860a4bcf2179881d61d2583e016f43a9e13c47bc
Greg Fodor <gfodor@gmail.com>
2020-11-10 16:49:19 -0800

    Take out trap for now

--- scripts/hab-build-and-push.sh
+++ scripts/hab-build-and-push.sh
@@ -22,7 +22,8 @@ PKG="$pkg_origin/$pkg_name"
 
 pushd "$DIR/.."
 
-trap "rm /hab/svc/$pkg_name/var/deploying && sudo /usr/bin/hab-clean-perms && chmod -R a+rw ." EXIT
+#trap "rm /hab/svc/$pkg_name/var/deploying && sudo /usr/bin/hab-clean-perms && chmod -R a+rw ." EXIT
+trap "sudo /usr/bin/hab-clean-perms && chmod -R a+rw ." EXIT
 
 # Wait for a lock file so we serialize deploys
 mkdir -p /hab/svc/$pkg_name/var
