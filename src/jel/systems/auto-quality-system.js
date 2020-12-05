ae61266793addc50134b97248179e4b62abdc8b7
Greg Fodor <gfodor@gmail.com>
2020-12-04 16:53:45 -0800

    Remove debug shim, use two consecutive fast frames as test for quality

--- src/jel/systems/auto-quality-system.js
+++ src/jel/systems/auto-quality-system.js
@@ -2,36 +2,19 @@ const SAMPLING_DURATION_MS = 1000.0 * 4.0;
 const LOWER_QUALITY_FRAME_LENGTH = 1000.0 / 40; // Ensure at least 50 FPS
 const MIN_SAMPLES_NEEDED = 15;
 const RESET_ON_TIME_JUMP_MS = 2000.0;
-const DEBUG_DURATION_MS = 20000.0;
-
-import qsTruthy from "../../hubs/utils/qs_truthy";
-const debugAutoQuality = qsTruthy("debug_auto_quality");
+const MIN_NUM_CONSECUTIVE_FAST_FRAMES = 2;
 
 // Conservative quality adjuster, take frames during a SAMPLING_DURATION_MS
-// period and if all frames fail to run fast enough (LOWER_QUALITY_FRAME_LENGTH)
-// then lower quality.
+// period and if we never see MIN_NUM_CONSECUTIVE_FAST_FRAMES consecutive fast frames then lower quality.
 export class AutoQualitySystem {
   constructor(sceneEl) {
     this.scene = sceneEl;
     this.enableTracking = false;
-    this.sawFastFrame = false;
-    this.slowFrames = 0;
+    this.numConsecutiveFastFrames = 0;
+    this.metFastFrameTest = false;
     this.sampledFrames = 0;
     this.lastTick = 0;
     this.debugStartTime = performance.now();
-  }
-
-  debugLog() {
-    if (!debugAutoQuality) return;
-    if (performance.now() - this.debugStartTime > DEBUG_DURATION_MS) {
-      if (window.APP.detailLevel !== 2) {
-        window.APP.detailLevel = 2;
-        console.log("Quality debug complete. Dropping quality level.");
-      }
-      return;
-    }
-
-    console.log(...arguments);
   }
 
   startTracking() {
@@ -45,34 +28,36 @@ export class AutoQualitySystem {
     this.enableTracking = false;
   }
 
-  tick(t) {
+  tick() {
     if (!this.enableTracking) return;
     if (this.lastTick === 0) {
       this.lastTick = performance.now();
       return;
     }
 
-    const dt = performance.now() - this.lastTick;
-    this.lastTick = performance.now();
+    const now = performance.now();
+    const dt = now - this.lastTick;
+    this.lastTick = now;
 
     this.sampledFrames++;
 
     if (dt > RESET_ON_TIME_JUMP_MS) {
       // Process was likely suspended
       this.timeSinceLastCheck = 0;
-      this.sawFastFrame = false;
-
-      this.debugLog("Adjust for suspend at ", t);
+      this.metFastFrameTest = false;
+      this.numConsecutiveFastFrames = 0;
 
       return;
     }
 
     if (dt < LOWER_QUALITY_FRAME_LENGTH) {
-      this.sawFastFrame = true;
+      this.consecutiveFastFrames++;
 
-      this.debugLog("Fast frame at ", t, dt);
+      if (this.consecutiveFastFrames >= MIN_NUM_CONSECUTIVE_FAST_FRAMES) {
+        this.metFastFrameTest = true;
+      }
     } else {
-      this.slowFrames++;
+      this.consecutiveFastFrames = 0;
     }
 
     this.timeSinceLastCheck += dt;
@@ -80,14 +65,11 @@ export class AutoQualitySystem {
     if (this.timeSinceLastCheck > SAMPLING_DURATION_MS) {
       this.timeSinceLastCheck = 0;
 
-      if (this.sampledFrames < MIN_SAMPLES_NEEDED) {
-        this.debugLog("Insufficient samples at ", t, dt);
-        return;
-      }
+      if (this.sampledFrames < MIN_SAMPLES_NEEDED) return;
 
       this.sampledFrames = 0;
 
-      if (!this.sawFastFrame) {
+      if (!this.metFastFrameTest) {
         window.APP.detailLevel = Math.min(2, window.APP.detailLevel + 1);
         console.warn("Slow framerate detected New detail level: ", window.APP.detailLevel);
 
@@ -96,8 +78,8 @@ export class AutoQualitySystem {
         document.body.classList.add("low-detail");
       }
 
-      this.sawFastFrame = false;
-      this.slowFrames = 0;
+      this.consecutiveFastFrames = 0;
+      this.metFastFrameTest = false;
     }
   }
 }
