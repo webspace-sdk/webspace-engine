c86a01b2aee248598e0713406554393a80f0124f
Greg Fodor <gfodor@gmail.com>
2021-02-24 09:16:16 -0800

    Mising closing head tag

--- src/jel/react-components/notifications-popup.stories.js
+++ src/jel/react-components/notifications-popup.stories.js
@@ -9,6 +9,20 @@ const subscriptions = new Subscriptions();
 export const Space = () => {
   useEffect(() => document.querySelector(`.${sharedStyles.showWhenPopped}`).focus());
 
+  subscriptions.subscribed = false;
+
+  return (
+    <div>
+      <SpaceNotificationsPopup styles={{}} attributes={{}} subscriptions={subscriptions} />
+    </div>
+  );
+};
+
+export const SpaceSubscribed = () => {
+  useEffect(() => document.querySelector(`.${sharedStyles.showWhenPopped}`).focus());
+
+  subscriptions.subscribed = true;
+
   return (
     <div>
       <SpaceNotificationsPopup styles={{}} attributes={{}} subscriptions={subscriptions} />
@@ -19,9 +33,33 @@ export const Space = () => {
 export const Hub = () => {
   useEffect(() => document.querySelector(`.${sharedStyles.showWhenPopped}`).focus());
 
+  subscriptions.subscribed = false;
   return (
     <div>
-      <HubNotificationsPopup styles={{}} attributes={{}} hub={{ name: "My Hub" }} subscriptions={subscriptions} />
+      <HubNotificationsPopup
+        styles={{}}
+        attributes={{}}
+        hub={{ hub_id: "abc123", name: "My Hub" }}
+        subscriptions={subscriptions}
+        hubSettings={{ abc123: { notify_joins: true } }}
+      />
+    </div>
+  );
+};
+
+export const HubSubscribed = () => {
+  useEffect(() => document.querySelector(`.${sharedStyles.showWhenPopped}`).focus());
+  subscriptions.subscribed = true;
+
+  return (
+    <div>
+      <HubNotificationsPopup
+        styles={{}}
+        attributes={{}}
+        hub={{ hub_id: "abc123", name: "My Hub" }}
+        subscriptions={subscriptions}
+        hubSettings={{ abc123: { notify_joins: true } }}
+      />
     </div>
   );
 };
