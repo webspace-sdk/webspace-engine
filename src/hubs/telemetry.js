df891ace105c568c10c276e204d03aab99089cbe
Greg Fodor <gfodor@gmail.com>
2021-02-24 10:02:09 -0800

    More attempts at fixing GA

--- src/hubs/telemetry.js
+++ src/hubs/telemetry.js
@@ -15,15 +15,19 @@ export default function registerTelemetry() {
   }
 
   if (gaTrackingId) {
-    window.dataLayer = window.dataLayer || [];
-
-    gtag("js", new Date());
-    gtag("config", gaTrackingId);
+    console.log("Tracking: GA Tracking ID: " + gaTrackingId);
 
     const el = document.createElement("script");
     el.type = "text/javascript";
     el.setAttribute("async", "true");
     el.setAttribute("src", `https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`);
+    el.addEventListener("load", () => {
+      window.dataLayer = window.dataLayer || [];
+
+      gtag("js", new Date());
+      gtag("config", gaTrackingId);
+    });
+
     document.documentElement.firstChild.appendChild(el);
   }
 }
