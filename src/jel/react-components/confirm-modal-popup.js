60662625dad8663d4d2bc895e3d46653e9075542
Greg Fodor <gfodor@gmail.com>
2021-03-12 22:53:05 -0800

    Added a hacky background layer to modal

--- src/jel/react-components/confirm-modal-popup.js
+++ src/jel/react-components/confirm-modal-popup.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import React, { forwardRef } from "react";
+import styled from "styled-components";
 import ReactDOM from "react-dom";
 import ConfirmModalPanel from "./confirm-modal-panel";
 import { waitForDOMContentLoaded } from "../../hubs/utils/async-utils";
@@ -7,6 +8,19 @@ import sharedStyles from "../../assets/jel/stylesheets/shared.scss";
 
 let popupRoot = null;
 waitForDOMContentLoaded().then(() => (popupRoot = document.getElementById("jel-popup-root")));
+
+// Extremely hacky modal overlay background
+// Modal is not centered so need 2x spillover
+const Background = styled.div`
+  position: absolute;
+  width: 200vw;
+  height: 200vh;
+  top: -100vh;
+  left: -100vw;
+  background: rgba(0, 0, 0, 0.6);
+  pointer-events: none;
+  z-index: -1;
+`;
 
 const ConfirmModalPopup = forwardRef(({ styles, attributes, setPopperElement, atomId, atomMetadata }, ref) => {
   const popupInput = (
@@ -17,6 +31,7 @@ const ConfirmModalPopup = forwardRef(({ styles, attributes, setPopperElement, at
       style={styles.popper}
       {...attributes.popper}
     >
+      <Background />
       <ConfirmModalPanel
         className={sharedStyles.slideDownWhenPopped}
         atomId={atomId}
