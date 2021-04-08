aa85c78ac7cfde7f8c33fe45b3c8c65686deaee1
Greg Fodor <gfodor@gmail.com>
2021-04-08 10:08:41 -0700

    Add proper fetch handler for offline mode

--- src/jel.service.js
+++ src/jel.service.js
@@ -13,7 +13,33 @@ self.addEventListener("activate", function(e) {
   return e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", function() {});
+self.addEventListener("fetch", function(event) {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          // First, try to use the navigation preload response if it's supported.
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+
+          // Always try the network first.
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        } catch (error) {
+          // catch is only triggered if an exception is thrown, which is likely
+          // due to a network error.
+          // If fetch() returns a valid HTTP response with a response code in
+          // the 4xx or 5xx range, the catch() will NOT be called.
+          console.log("Fetch failed; returning offline page instead.", error);
+          const offline = { status: 200, statusText: "Worker is offline" };
+          return new Response("Jel offline mode is not supported.", offline);
+        }
+      })()
+    );
+  }
+});
 
 self.addEventListener("push", function(e) {
   const payload = JSON.parse(e.data.text());
