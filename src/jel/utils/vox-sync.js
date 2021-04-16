f2d0131a6901fa42c502dc21b8fb5496867d31bb
Greg Fodor <gfodor@gmail.com>
2021-04-16 14:15:57 -0700

    Basic vox mutations working and synced

--- src/jel/utils/vox-sync.js
+++ src/jel/utils/vox-sync.js
@@ -1,6 +1,9 @@
 import jwtDecode from "jwt-decode";
 import { EventTarget } from "event-target-shim";
-import { VoxChunk } from "ot-vox";
+import { VoxSnapshot, VoxChunk, voxColorForRGBT, VOXEL_TYPE_DIFFUSE, REMOVE_VOXEL_COLOR } from "ot-vox";
+
+const MAX_FRAMES = 32;
+const INITIAL_FRAME_SIZE = 32;
 
 export default class VoxSync extends EventTarget {
   constructor(voxId) {
@@ -23,17 +26,44 @@ export default class VoxSync extends EventTarget {
     await new Promise(res => {
       doc.subscribe(() => {
         doc.on("op", this.handleDocOp);
-
-        // Frames
-        if (!doc.data.f) {
-          doc.submitOp([{ p: ["f"], oi: [] }]);
-          doc.submitOp([{ p: ["f", 0], li: null }]);
-          doc.submitOp([{ p: ["f", 0], t: "vox0", o: { d: new VoxChunk(32).serialize() } }]);
-        }
-
         res();
       });
     });
+  }
+
+  ensureFrame(idxFrame) {
+    if (idxFrame > MAX_FRAMES - 1) return;
+    const snapshot = this._doc.data;
+
+    if (snapshot.frames[idxFrame]) return;
+
+    const indices = new Array(INITIAL_FRAME_SIZE ** 3);
+    indices.fill(0);
+
+    const delta = VoxChunk.fromJSON({
+      size: INITIAL_FRAME_SIZE,
+      palette: [],
+      indices
+    });
+
+    this._doc.submitOp({ f: idxFrame, d: delta.serialize() });
+  }
+
+  setVoxel(x, y, z, r, g, b, frame = 0) {
+    this.ensureFrame(0);
+
+    const color = voxColorForRGBT(r, g, b, VOXEL_TYPE_DIFFUSE);
+    const delta = VoxChunk.fromJSON({ size: 1, palette: [color], indices: [1] });
+    const op = { f: frame, d: delta.serialize() };
+
+    this._doc.submitOp(op);
+  }
+
+  removeVoxel(x, y, z, frame = 0) {
+    this.ensureFrame(0);
+
+    const delta = VoxChunk.fromJSON({ size: 1, palette: [REMOVE_VOXEL_COLOR], indices: [1] });
+    this._doc.submitOp({ f: frame, d: delta.serialize() });
   }
 
   handleDocOp(o, source) {
