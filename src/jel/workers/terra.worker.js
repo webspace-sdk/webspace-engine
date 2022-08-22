13f2daa25e04eb3b78f8feddebd4db1cffa42985
Greg Fodor <gfodor@gmail.com>
2022-08-21 22:47:37 -0700

    Store chunks in indexdb

--- src/jel/workers/terra.worker.js
+++ src/jel/workers/terra.worker.js
@@ -2,12 +2,25 @@ let world = null;
 import blockTypes from "../terra/blocks";
 import World from "../terra/world";
 
+const CHUNK_VERSION = 1;
+
 const WORLD_TYPE_GENERATORS = {
   1: "islands",
   2: "hilly",
   3: "flat",
   4: "flat"
 };
+
+let db;
+const req = indexedDB.open("terra", 1);
+
+req.addEventListener("success", ({ target: { result } }) => {
+  db = result;
+});
+
+req.addEventListener("upgradeneeded", ({ target: { result: db } }) => {
+  db.createObjectStore("chunks", { keyPath: "id" });
+});
 
 self.onmessage = ({
   data: {
@@ -21,16 +34,34 @@ self.onmessage = ({
     return;
   }
 
-  if (world === null || world.seed !== seed || world.generatorType !== type) {
-    world = new World({
-      blockTypes,
-      generatorType,
-      seed
-    });
-  }
+  const chunkId = `${type}/${seed}/${x}/${z}/${CHUNK_VERSION}`;
 
-  self.postMessage({
-    id,
-    result: world.getEncodedChunk(x, z)
+  const txn = db
+    .transaction("chunks")
+    .objectStore("chunks")
+    .get(chunkId);
+
+  txn.addEventListener("success", ({ target }) => {
+    if (target.result) {
+      self.postMessage({ id, result: target.result.chunk });
+    } else {
+      if (world === null || world.seed !== seed || world.generatorType !== generatorType) {
+        world = new World({
+          blockTypes,
+          generatorType,
+          seed
+        });
+      }
+
+      const chunk = world.getEncodedChunk(x, z);
+      self.postMessage({
+        id,
+        result: chunk
+      });
+
+      db.transaction("chunks", "readwrite")
+        .objectStore("chunks")
+        .put({ id: chunkId, chunk });
+    }
   });
 };
